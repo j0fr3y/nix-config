a8c2fe83b682d629cafaeb5f3b91460b0beb9139
Jonas Frey <46280748+j0fr3y@users.noreply.github.com>
2026-02-10 11:51:11 +0100

    Remove custom Neovim configuration
    Removed custom configuration and plugin settings for Neovim.

--- modules/applications/neovim.nix
+++ modules/applications/neovim.nix
@@ -7,41 +7,6 @@
     viAlias = true;
     vimAlias = true;
     
-    configure = {
-      customRC = ''
-        " Basic settings
-        set number
-        set relativenumber
-        set tabstop=2
-        set shiftwidth=2
-        set expandtab
-        set smartindent
-        set mouse=a
-        set clipboard=unnamedplus
-        
-        " Search settings
-        set ignorecase
-        set smartcase
-        set incsearch
-        set hlsearch
-        
-        " UI settings
-        set termguicolors
-        set signcolumn=yes
-        set cursorline
-        
-        " Colorscheme
-        colorscheme slate
-      '';
-      
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [
-          vim-nix
-          vim-commentary
-          vim-surround
-          vim-fugitive
-        ];
-      };
-    };
+    
   };
 }
